052cce2694a2bdb85124f2f3839a05d61a76f86c
AttEela <eelaman@hotmail.fr>
2019-06-01 16:12:35 +0200

    change architecture with environ var

--- logger.py
+++ logger.py
@@ -1,30 +1,26 @@
+import os
 import logging
-# import os
-
 from logging.handlers import RotatingFileHandler
 
-# création de l'objet logger qui va nous servir à écrire dans les logs
-logger = logging.getLogger()
-logger_level = "DEBUG"  # os.environ.get("LOGGER_LEVEL")
-path_to_activity = "./" + "logs" + "/"  # os.environ.get("LOG_PATH") + "/"
-# on met le niveau du logger à DEBUG, comme ça il écrit tout
-logger.setLevel(getattr(logging, logger_level))
+logger_level = os.environ.get("LOG_LEVEL")
+log_format = os.environ.get("LOG_FORMAT")
+path_to_activity_log = os.path.join(".",
+                                    os.environ.get("LOG_PATH"),
+                                    'server_activity.log')
 
-# création d'un formateur qui va ajouter le temps, le niveau
-# de chaque message quand on écrira un message dans le log
-formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(filename)s :: %(message)s')
-# création d'un handler qui va rediriger une écriture du log vers
-# un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
-file_handler = RotatingFileHandler(path_to_activity + 'server_activity.log',
-                                   'a', 10000000, 1)
-# on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
-# créé précédement et on ajoute ce handler au logger
-file_handler.setLevel(getattr(logging, logger_level))
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
 
-# création d'un second handler qui va rediriger chaque écriture de log
-# sur la console
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(getattr(logging, logger_level))
-logger.addHandler(stream_handler)
+class LogManager:
+    def __init__(self, logger_level=logger_level, path_to_activity=path_to_activity_log):
+        self.logger = logging.getLogger()
+        self.logger.setLevel(getattr(logging, logger_level))
+        self.formatter = logging.Formatter(log_format)
+        self.file_handler = RotatingFileHandler(filename=path_to_activity_log,
+                                                mode='a',
+                                                maxBytes=10000,
+                                                backupCount=1)
+        self.file_handler.setLevel(getattr(logging, logger_level))
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+        self.stream_handler = logging.StreamHandler()
+        self.stream_handler.setLevel(getattr(logging, logger_level))
+        self.logger.addHandler(self.stream_handler)
